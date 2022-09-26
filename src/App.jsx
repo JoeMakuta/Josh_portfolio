411790f1492d4144374cf924d99a7c4eababea51
Josue Makuta <makutajosue@gmail.com>
2022-09-26 10:38:38 +0200

    Feat : The body_2 design is done ...

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import TopMenu from './components/top_menu'
 import Body_1 from './components/body_1'
 import './App.css'
 import Body_1_alter from './components/body_1'
+import Body_2 from './components/Body_2'
 
 function App() {
   return (
     <div className="font-poppins bg-main_color bg-opacity-30 ">
       <Body_1_alter />
+      <Body_2 />
     </div>
   )
 }
